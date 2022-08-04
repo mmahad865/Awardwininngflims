3e7fce42f717b900dc94d992b3901c6b3e774799
mahad Ali <mmahad865@gmail.com>
2022-08-03 17:27:24 -0700

    saved javascript

---
+++ style.js
@@ -0,0 +1,19 @@
+const Owner = "Mahad"
+const instructor = "Kassie"
+
+let response = prompt ("Hi, Please enter your name")
+document.write("Hello"  + response , "What's your favorite movie")
+console.log(response);
+
+if (response == Owner) {
+    document.write("Hi there Mahad");}
+else if (response == instructor){
+    document.write("Hi there Kassie");}
+else {
+    document.write("Welcome to my page");
+
+    }
+    
+    
+
+    
