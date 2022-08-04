7185ddc15fe34a69d622603bfb031eebdc6b7316
mahad Ali <mmahad865@gmail.com>
2022-08-04 16:40:35 -0700

    more css

--- style.js
+++ style.js
@@ -1,8 +1,8 @@
 const Owner = "Mahad"
 const instructor = "Kassie"
 
-let response = prompt ("Hi, Please enter your name")
-document.write("Hello"  + response , "What's your favorite movie")
+let response = prompt ("Hi, Please enter your name  ")
+document.write("Hello"  + response , "  What's your favorite movie")
 console.log(response);
 
 if (response == Owner) {
@@ -11,8 +11,19 @@ else if (response == instructor){
     document.write("Hi there Kassie");}
 else {
     document.write("Welcome to my page");
+}
 
-    }
+
+let myFavMovie = "The matrix"
+response = prompt ("Whats your favorite movie ")
+
+while (response !== myFavMovie){
+    response = prompt("what's my favorite movie?");
+    if (response !== myFavMovie){
+    alert("you're wrong, enter The matrix")
+}
+}
+
     
     
 
